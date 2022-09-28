cc222712226dca25ce18d5ba7b64b77945cfc24d
Jorge Osses <jorgitoosses@gmail.com>
2022-09-27 23:03:15 -0300

    clase 10

---
+++ backend/core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+
+def home(request):
+    return render(request, "core/home.html")
+    # n = 1
+    # html_response = "<h1>Mi web</h1>"
+    # for i in range(10):
+    #     html_response += f"<h2>Esta es la portada N°{n}</h2>"
+    #     n += 1
+    # return HttpResponse(html_response)
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+
+def contact(request):
+    return render(request, "core/contact.html")
